267b4a9e3cb3b1d367ac3c02dc48781eb2297bfb
Ashok Fernandez <ashok.fernandez@gmail.com>
2023-05-17 18:03:05 +0200

    Drawing indicator dots

--- Source/SmallKnobLookAndFeel.cpp
+++ Source/SmallKnobLookAndFeel.cpp
@@ -33,7 +33,10 @@ void SmallKnobLookAndFeel::drawRotarySlider (juce::Graphics& g, int x, int y, in
 
     // Calculate scale ratio for shrinking the rotating layer on top of the background 
     // comes from the size of the two layers in the Figma designs
-    float scaleRatio = 266.0f / 488.0f;
+    float rotationLayerWidth = 266.0f;
+    float backgroundLayerWidth = 488.0f;
+    float scaleRatio = rotationLayerWidth / backgroundLayerWidth;
+
     int rotWidth = int(width * scaleRatio);
     int rotHeight = int(height * scaleRatio);
     int offsetX = (width - rotWidth) / 2;
@@ -42,6 +45,51 @@ void SmallKnobLookAndFeel::drawRotarySlider (juce::Graphics& g, int x, int y, in
     // Calculate rotation for the rotating layer
     float customRange = 3 * juce::MathConstants<float>::pi / 2; // range from -3*pi/4 to 3*pi/4 is 1.5*pi
     float customAngle = (sliderPos - 0.5f) * customRange; // sliderPos - 0.5f ranges from -0.5 to 0.5, so this gives a range from -3*pi/4 to 3*pi/4
+
+    // FIRST DRAW THE INDICATOR DOTS AROUND THE PERMIETER OF THE DIAL
+
+    // Draw indicator dots
+    int numOfDots = 15; // variable to hold the number of dots
+    float radius = (width - rotWidth) * 0.8; // radius for the invisible circle
+    float totalRange = customRange;  // range from -3*pi/4 to 3*pi/4 is 1.5*pi
+    float angleStep; // calculate the angular step for each dot
+    if (numOfDots == 3) {
+        angleStep = totalRange / 2;
+    } else {
+        angleStep = totalRange / (numOfDots - 2);
+    }
+    float dotDiameter = 5; // diameter of the dots
+
+    // Indicator dots around the perimeter of the dial 
+    for (int i = 0; i < numOfDots; ++i)
+    {
+        // Calculate angle for each dot
+        float dotAngle;
+        if (i == 0) {
+            dotAngle = -3 * juce::MathConstants<float>::pi / 4;
+        } else if (i == numOfDots - 1) {
+            dotAngle = (3 * juce::MathConstants<float>::pi / 4) - 0.00001; // Take a tiny bit off so the final dot lights up
+        } else {
+            dotAngle = -3 * juce::MathConstants<float>::pi / 4 + (i * angleStep);
+        }
+
+        // Adjust the angle due to the coordinate system of JUCE
+        float adjustedDotAngle = dotAngle - juce::MathConstants<float>::pi / 2;
+
+        // Calculate dot position
+        int dotX = x + width / 2 + radius * std::cos(adjustedDotAngle) - dotDiameter / 2;
+        int dotY = y + height / 2 + radius * std::sin(adjustedDotAngle) - dotDiameter / 2;
+
+
+        // Choose color based on the comparison between dotAngle and customAngle
+        juce::Colour dotColor = dotAngle < customAngle ? juce::Colours::yellow : juce::Colours::green;
+
+        // Draw the dot
+        g.setColour(dotColor);
+        g.fillEllipse(dotX, dotY, dotDiameter, dotDiameter);
+    }
+
+    // DRAW THE DIAL ROTATED, WITH A DROP SHADOW
 
     // Create a DropShadow
     juce::DropShadow dropShadow;
